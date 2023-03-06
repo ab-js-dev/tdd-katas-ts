f90e18800c149a942299a287fc7e53f0da492a98
ab-js-dev <101796494+ab-js-dev@users.noreply.github.com>
2023-03-07 03:34:39 +0400

    cycle 1

---
+++ src/katas/xmas-lights-kata.test.ts
@@ -0,0 +1,10 @@
+import { XmasLights } from "./xmas-lights-kata";
+describe('Xmas light Kata tests', () => {
+
+    it('should create an instance of class', () => {
+        expect(new XmasLights()).toBeTruthy()
+    })
+
+
+
+})
