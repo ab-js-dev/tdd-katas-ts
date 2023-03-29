09cc55c0855625d435aff37359e69d9d99d3f388
ab-js-dev <101796494+ab-js-dev@users.noreply.github.com>
2023-03-29 22:53:44 +0400

    cycle 1

---
+++ src/katas/credit-card-checker.test.ts
@@ -0,0 +1,6 @@
+import { validCard } from './credit-card-checker';
+describe('', () => {
+    it('should validCard function be defined', () => {
+        expect(validCard).toBeDefined()
+    });
+})
---
+++ src/katas/credit-card-checker.ts
@@ -0,0 +1,3 @@
+export const validCard = () => {
+    return null
+}
