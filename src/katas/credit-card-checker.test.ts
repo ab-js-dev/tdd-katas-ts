1797309444789f273ecbaca0afca7995c93205bf
ab-js-dev <101796494+ab-js-dev@users.noreply.github.com>
2023-03-29 23:46:55 +0400

    cycle 3

--- src/katas/credit-card-checker.test.ts
+++ src/katas/credit-card-checker.test.ts
@@ -9,4 +9,7 @@ describe('', () => {
     it('should validCard function be defined', () => {
         expect(validCard('5457 6238 9323 4311')).toEqual(false)
     });
+    it('should validCard function be defined', () => {
+        expect(validCard('9999 9999 9999 9995')).toEqual(true)
+    });
 })
--- src/katas/credit-card-checker.ts
+++ src/katas/credit-card-checker.ts
@@ -1,3 +1,9 @@
 export const validCard = (cardNumber: string) => {
-    return cardNumber === '5457 6238 9823 4311'
+    const cleanNumbers = cardNumber.replace(/[^0-9]/g, '');
+    let sum = 0;
+    for(let i = 0; i < cleanNumbers.length; i++) {
+        const numberInProcess = i % 2 === 0 ?  Number(cleanNumbers[i]) * 2 :  Number(cleanNumbers[i]);
+        sum += (numberInProcess <= 9) ? numberInProcess : numberInProcess - 9;
+    }
+    return sum % 10 === 0;
 }
